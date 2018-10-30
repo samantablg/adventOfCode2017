1bf35f5b9de7e7bcf57a1f23e323820ab663c0df
Samanta <slaragiannone@gmail.com>
2018-10-30 14:11:46 +0000

    test added

--- tests/day2.test.js
+++ tests/day2.test.js
@@ -1,2 +1,7 @@
-
 const checkSum = require('../src/day2/index.js');
+
+describe("Check Sum", () => {
+    test("should return 18 for [5, 1, 9, 5],[7, 5, 3],[2, 4, 6, 8] ", () => {
+        expect(checkSum([5, 1, 9, 5],[7, 5, 3],[2, 4, 6, 8])).toBe(18);
+	});
+});
